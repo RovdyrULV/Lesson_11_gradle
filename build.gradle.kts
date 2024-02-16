c0ee995a6f64f4d6aa4ec411fce4e8be8fe20567
Rovdyr_UlV <rovdyrulv@gmail.com>
2024-02-16 17:37:04 +0300

    1

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation 'org.seleniumhq.selenium:selenium-java:4.17.0'
+    testImplementation 'org.junit.jupiter:junit-jupiter-engine:5.10.0'
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+
+    maxHeapSize = "1G"
+
+    testLogging {
+        events("passed")
+    }
+}
